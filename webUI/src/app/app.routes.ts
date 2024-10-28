3c9ebabd9e2d54845de3cb01219059cb4e830f58
Migas77 <miguel.belchior@ua.pt>
2024-10-28 17:18:38 +0000

    SCRUM-7 | feat: redirect login page, auth guards, auth service, consume backend endpoint, redirect to login if not logged in

--- webUI/src/app/app.routes.ts
+++ webUI/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
-import {LandingPageComponent} from "./landing-page/landing-page.component";
-import {RedirectLoginPageComponent} from "./redirect-login-page/redirect-login-page.component";
+import {LandingPageComponent} from "./components/landing-page/landing-page.component";
+import {RedirectLoginPageComponent} from "./components/redirect-login-page/redirect-login-page.component";
+import {HomeComponent} from "./components/home/home.component";
+import {AuthGuard} from "./guards/authGuard";
 
 export const routes: Routes = [
   {path: '', component: LandingPageComponent},
   {path: 'oauth2/idpresponse', component: RedirectLoginPageComponent},
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
 ];
