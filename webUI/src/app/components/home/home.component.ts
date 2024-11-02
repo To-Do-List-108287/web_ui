64847a29851dd3fc3fcaee7f67c29f71b8ea2bcc
Migas77 <miguel.belchior@ua.pt>
2024-11-02 11:59:42 +0000

    SCRUM-7 | feat: base to do lists with post-it style

--- webUI/src/app/components/home/home.component.ts
+++ webUI/src/app/components/home/home.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import {CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem} from "@angular/cdk/drag-drop";
+import {MatCard} from "@angular/material/card";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CdkDropList, CdkDrag, MatCard
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  todoTasks = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  inProgressTasks = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  doneTasks = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
 
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
