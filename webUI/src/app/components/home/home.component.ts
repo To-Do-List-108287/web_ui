435618135ff3e758f61636e2783d8bf8dcf983a8
Migas77 <miguel.belchior@ua.pt>
2024-11-10 19:41:50 +0000

    SCRUM-24 | feat: filter by category

--- webUI/src/app/components/home/home.component.ts
+++ webUI/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem} from "@angular/cdk/drag-drop";
 import {MatCard} from "@angular/material/card";
 import {MatDialog} from "@angular/material/dialog";
@@ -7,23 +7,27 @@ import {TaskResponse} from "../../models/TaskResponse";
 import {MatIcon} from "@angular/material/icon";
 import {TaskPriority} from "../../models/TaskPriority";
 import {MatMiniFabButton} from "@angular/material/button";
-import {DatePipe, NgClass, NgIf, NgStyle} from "@angular/common";
+import {DatePipe, NgClass, NgForOf, NgIf, NgStyle} from "@angular/common";
 import {DeleteTaskDialogComponent} from "../delete-task-dialog/delete-task-dialog.component";
 import {EditTaskDialogComponent} from "../edit-task-dialog/edit-task-dialog.component";
 import {TaskService} from "../../services/task.service";
 import {TaskCompletionStatus} from "../../models/TaskCompletionStatus";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {MatFormField, MatOption, MatSelect} from "@angular/material/select";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    CdkDropList, CdkDrag, MatCard, MatIcon, MatMiniFabButton, NgStyle, NgClass, DatePipe, NgIf
+    CdkDropList, CdkDrag, MatCard, MatIcon, MatMiniFabButton, NgStyle, NgClass, DatePipe, NgIf, MatSelect, MatFormField, MatOption, NgForOf,
+    MatFormFieldModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   readonly dialog = inject(MatDialog);
   private readonly _snackBar = inject(MatSnackBar);
   taskService = inject(TaskService);
@@ -40,10 +44,25 @@ export class HomeComponent {
     [TaskCompletionStatus.IN_PROGRESS]: this.inProgressTasks,
     [TaskCompletionStatus.DONE]: this.doneTasks
   }
+  selectedTaskCategory: string = '';
+  taskCategories: string[] = [];
 
-  constructor(private datePipe: DatePipe) {
-    this.taskService.getTasks().subscribe({
+  constructor(private datePipe: DatePipe, private route: ActivatedRoute, private router: Router) {
+    this.loadTasks(this.route.snapshot.queryParamMap.get('category'))
+    this.loadCategories()
+  }
+
+  ngOnInit(): void {
+    console.log('Home component initialized');
+  }
+
+
+  loadTasks(category: String | null = null) {
+    this.taskService.getTasks(category).subscribe({
       next: (tasks: TaskResponse[]) => {
+        this.todoTasks = [];
+        this.inProgressTasks = [];
+        this.doneTasks = [];
         tasks.forEach(task => {
           if (task.completionStatus === TaskCompletionStatus.TO_DO) {
             this.todoTasks.push(task);
@@ -56,9 +75,42 @@ export class HomeComponent {
       },
       error: err => {
         console.error(err);
+        this._snackBar.open('Error loading tasks. Try again later!', 'Close', {
+          duration: 2000,
+          panelClass: ['warning_snackbar']
+        });
       }
-
     })
+  }
+
+  loadCategories() {
+    this.taskService.getTaskCategories().subscribe({
+      next: (categories: string[]) => {
+        this.taskCategories = categories;
+        this.taskCategories.sort();
+        const passedCategory: string | null = this.route.snapshot.queryParamMap.get('category');
+        if (passedCategory) {
+          if (this.taskCategories.includes(passedCategory)) {
+            this.selectedTaskCategory = passedCategory;
+          } else {
+            this._snackBar.open("Category doesn't exist. Showing all tasks.", 'Close', {
+              duration: 2000,
+              panelClass: ['warning_snackbar']
+            });
+          }
+        }
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
+  }
+
+  updateCategories(newCategory: String){
+    const localTaskCategories: Set<String> = new Set<String>(this.taskCategories);
+    localTaskCategories.add(newCategory);
+    this.taskCategories = Array.from(localTaskCategories) as string[];
+    this.taskCategories.sort();
   }
 
   formatDate(dateString: string): string | null {
@@ -74,6 +126,7 @@ export class HomeComponent {
       next: (task: TaskResponse) => {
         if (task) {
           this.todoTasks.push(task);
+          this.updateCategories(task.category);
         }
       }
     });
@@ -95,10 +148,17 @@ export class HomeComponent {
   }
 
   openEditDialog(taskToEdit: TaskResponse) {
-    this.dialog.open(EditTaskDialogComponent, {
+    const dialogRef = this.dialog.open(EditTaskDialogComponent, {
       height: '618px',
       width: '618px',
       data: {task: taskToEdit}
+    });
+    dialogRef.afterClosed().subscribe({
+      next: (task: TaskResponse) => {
+        if (task) {
+          this.updateCategories(task.category);
+        }
+      }
     });
   }
 
@@ -168,6 +228,12 @@ export class HomeComponent {
         });
       }
     });
+  }
+
+
+  filterTasks() {
+    this.router.navigate(['home'], {queryParams: {category: this.selectedTaskCategory}});
+    this.loadTasks(this.selectedTaskCategory);
   }
 
   protected readonly Date = Date;
